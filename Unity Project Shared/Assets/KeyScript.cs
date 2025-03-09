eb4430981e37696ba5f8903753962b77bde6ba2a
maddymcw <11mcampbellwhite@brightoncollege.net>
2025-03-09 20:37:20 +0000

    key and doors working

--- Unity Project Shared/Assets/KeyScript.cs
+++ Unity Project Shared/Assets/KeyScript.cs
@@ -1,20 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class KeyScript : MonoBehaviour
 {
     public GameObject doorCollider;
+    public GameObject doorCollider2;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        doorCollider.SetActive(false); 
+        doorCollider.SetActive(true);
+        doorCollider2.SetActive(true);
     }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Player")
+        if(other.gameObject.tag == "Player")
         {
-            doorCollider.SetActive(true);
+            doorCollider.SetActive(false);
+            doorCollider2.SetActive(false);
             Destroy(gameObject);
         }
+        
     }
 }
